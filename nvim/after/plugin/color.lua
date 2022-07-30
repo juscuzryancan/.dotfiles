0b6a458faf4a3d0eb16efbfa2e7ca61b81897475
Ryan Riley Puzon <rrgpuzon@gmail.com>
2022-07-30 11:27:13 -0700

    creates init.lua

---
+++ nvim/after/plugin/color.lua
@@ -0,0 +1,7 @@
+print("hello from after")
+
+vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_transparent = true
+vim.opt.background = "dark"
+
+vim.cmd("colorscheme tokyonight")
---
+++ nvim/lua/juscuzryancan/init.lua
@@ -0,0 +1,1 @@
+require("juscuzryancan.set")
