c6d97c5362aa14b3f20865c90af164e6e019aaeb
Ryan Riley Puzon <rrgpuzon@gmail.com>
2022-07-31 02:51:47 -0700

    adds lsp and some more plugins

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,13 @@
+require'lspconfig'.gopls.setup{}
+
+require'lspconfig'.eslint.setup{}
+require'lspconfig'.html.setup{}
+require'lspconfig'.jsonls.setup{}
+require'lspconfig'.cssls.setup{}
+require'lspconfig'.cssmodules_ls.setup{}
+
+require'lspconfig'.tsserver.setup{}
+
+require'lspconfig'.prismals.setup{}
+
+require'lspconfig'.graphql.setup{}
--- nvim/lua/juscuzryancan/packer.lua
+++ nvim/lua/juscuzryancan/packer.lua
@@ -5,10 +5,49 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  use 'folke/tokyonight.nvim'
+
+  --random dependencies
   use 'nvim-lua/plenary.nvim'
+
+  --color schemes
+  use 'folke/tokyonight.nvim'
+
+  --telescope
   use { 
     'nvim-telescope/telescope.nvim', branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+  --toggle term to open up new terminal windows in the nvim window
+  use {"akinsho/toggleterm.nvim", tag = 'v2.*', config = function()
+    require("toggleterm").setup()
+  end}
+
+  --lsp ui window thingy
+  use({
+    "glepnir/lspsaga.nvim",
+    branch = "main",
+    config = function()
+      local saga = require("lspsaga")
+
+      saga.init_lsp_saga({
+        -- your configuration
+      })
+    end,
+  })
+
+  --lsp
+  use 'neovim/nvim-lspconfig'
+
+  --airline
+  use 'vim-airline/vim-airline' 
+
+  --git-gutter
+  use 'airblade/vim-gitgutter'
+
+  --fugitive
+  use 'tpope/vim-fugitive'
+
+  --surroudn
+  use 'tpope/vim-surround'
 end)
